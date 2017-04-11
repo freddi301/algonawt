2f1a8ca9eb84848e8a4a9503bfc98354a4cb8b9f
Frederik Batuna <gobi301@gmail.com>
2017-04-11 04:03:26 -0700

    try to add nesting semantics to slimmer reducer

--- test/reducer2.spec.js
+++ test/reducer2.spec.js
@@ -25,6 +25,10 @@ export function red<State>(instructions: Array<(s: State) => State>): (s: State)
   return state => instructions.reduce((state, instruction) => instruction(state), state);
 }
 
+const nest = (outerAction, innerSelector, innerAction, value) => {
+  return outer => outerAction(innerAction(value)(innerSelector(outer)))(outer);
+};
+
 const counter = {
   inc: n => s => s + n,
   dec: n => s => s - n
@@ -46,10 +50,9 @@ describe('light reducer', () => {
       expect(red(subroutine2)(-6)).to.equal(0);
     });
   });
-
   describe('run', () => {
+    const id = x => x;
     it('works', () => {
-      const id = x => x;
       function * calculatrix ({ inc, dec }) {
         yield inc(2);
         yield dec((yield id) * 3);
@@ -57,5 +60,51 @@ describe('light reducer', () => {
       const result = run(calculatrix(counter))(-1);
       expect(result).to.equal(-2);
     });
+  });
+  describe('nested classes', () => {
+    class Person {
+      name: string = 'fred';
+      clothes: Suit = new Suit;
+      static rename = (name: string) => (p: Person): Person => {
+        const ret = new Person;
+        ret.name = name;
+        ret.clothes = p.clothes;
+        return ret;
+      }
+      static changeClothes = (clothes: Suit) => (p: Person): Person => {
+        const ret = new Person;
+        ret.name = p.name;
+        ret.clothes = clothes;
+        return ret;
+      }
+      static clothes = (p: Person): Suit => p.clothes;
+    }
+    class Suit {
+      pants: Pants = new Pants;
+      tShirt: TShirt = new TShirt;
+      static changePants = (pants: Pants) => (s: Suit): Suit => {
+        const ret = new Suit;
+        ret.tShirt = s.tShirt;
+        ret.pants = pants;
+        return ret;
+      }
+    }
+    class Pants {} class TShirt {} class Skirt extends Pants {}
+    const { rename, changeClothes, changePants, clothes } = { ...Person, ...Suit };
+    const bob = red([
+      rename('bob'),
+      bob => changeClothes(
+        changePants(new Skirt)(bob.clothes)
+      )(bob)
+    ])(new Person);
+    expect(bob.name).to.equal('bob');
+    expect(bob.clothes.pants).to.be.instanceof(Skirt);
+    //const id = x => x;
+    const bobby = run(function * () {
+      yield rename('bobby');
+      yield nest(changeClothes, clothes, changePants, new Skirt);
+    }())(new Person);
+    expect(bobby.name).to.equal('bobby');
+    expect(bobby.clothes.pants).to.be.instanceof(Skirt);
   });
 });
